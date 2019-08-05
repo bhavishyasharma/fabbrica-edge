086c5015ca9083425649cdde596aecd9920b0b67
Bhavishya Sharma <bhavishyasharma@live.com>
2019-08-05 17:56:20 +0530

    ScheduleTask based on Measurements

--- src/main/java/in/co/futech/fabbricaedge/FabbricaEdgeApplication.java
+++ src/main/java/in/co/futech/fabbricaedge/FabbricaEdgeApplication.java
@@ -1,41 +1,84 @@
 package in.co.futech.fabbricaedge;
 
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
 import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
 import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 
 @SpringBootApplication
 @IntegrationComponentScan
 @EnableScheduling
-public class FabbricaEdgeApplication {
+@EnableIntegration
+public class FabbricaEdgeApplication implements ApplicationContextAware {
+
+	private ApplicationContext applicationContext;
+
+	@Autowired
+	private TaskScheduler taskScheduler;
 
 	@Autowired
 	private EdgeConfiguration edgeConfiguration;
 
 	private static Random random = new Random();
 
+	private List<ScheduledTask> scheduledTasks;
+
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(FabbricaEdgeApplication.class)
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(FabbricaEdgeApplication.class)
 				.web(WebApplicationType.NONE)
 				.run(args);
+	}
+
+	@PostConstruct
+	public void initializeTasks() {
+		HashMap<Integer, List<EdgeConfiguration.Measurement>> hashMap = new HashMap<Integer, List<EdgeConfiguration.Measurement>>();
+		for(EdgeConfiguration.Measurement measurement: edgeConfiguration.getMeasurements()) {
+			if(!hashMap.containsKey(measurement.getFrequency())) {
+				List<EdgeConfiguration.Measurement> list = new ArrayList<EdgeConfiguration.Measurement>();
+				list.add(measurement);
+				hashMap.put(measurement.getFrequency(), list);
+			}
+			else {
+				hashMap.get(measurement.getFrequency()).add(measurement);
+			}
+		}
+		this.scheduledTasks = new ArrayList<>();
+		for (HashMap.Entry<Integer, List<EdgeConfiguration.Measurement>> measurements : hashMap.entrySet()) {
+			ScheduledTask scheduledTask = applicationContext.getBean(ScheduledTask.class , measurements.getValue());
+			taskScheduler.scheduleAtFixedRate(new Runnable() {
+				@Override
+				public void run() {
+					scheduledTask.runTask();
+				}
+			}, measurements.getKey());
+			scheduledTasks.add(scheduledTask);
+		}
 	}
 
 	@Bean
@@ -64,4 +107,8 @@ public class FabbricaEdgeApplication {
 		return new DirectChannel();
 	}
 
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
 }
--- src/main/java/in/co/futech/fabbricaedge/ScheduledTask.java
+++ src/main/java/in/co/futech/fabbricaedge/ScheduledTask.java
@@ -2,12 +2,21 @@ package in.co.futech.fabbricaedge;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.SchedulingConfiguration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @Component
+@Scope("prototype")
 public class ScheduledTask {
 
     @Autowired
@@ -16,10 +25,15 @@ public class ScheduledTask {
     @Autowired
     private EdgeConfiguration edgeConfiguration;
 
+    private List<EdgeConfiguration.Measurement> measurements;
+
     private Random random = new Random();
 
-    @Scheduled(fixedRate = 1000)
-    public void run() {
-        mqttGateway.send("fabbrica,topic="+ edgeConfiguration.getClientid() +" oilTemperature="+(random.nextInt(10)+30));
+    public ScheduledTask(List<EdgeConfiguration.Measurement> measurements) {
+        this.measurements = measurements;
+    }
+
+    public void runTask() {
+        mqttGateway.send("fabbrica,topic="+ edgeConfiguration.getClientid() + " " + measurements.get(0).getName() + "="+(random.nextInt(10)+30));
     }
 }
